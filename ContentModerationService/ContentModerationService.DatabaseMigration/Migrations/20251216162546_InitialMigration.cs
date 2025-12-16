874e37cd80ea5becafe5a550ec4f5271628609ab
Bilal Kinali <biki59986@edu.ucl.dk>
2025-12-16 17:27:56 +0100

    Added dependency and migration - database table created successfully

---
+++ ContentModerationService/ContentModerationService.DatabaseMigration/Migrations/20251216162546_InitialMigration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ContentModerationService.DatabaseMigration.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ModerationDecisions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CorrelationId = table.Column<string>(type: "text", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    SuggestedAction = table.Column<int>(type: "integer", nullable: false),
+                    Hate = table.Column<int>(type: "integer", nullable: true),
+                    SelfHarm = table.Column<int>(type: "integer", nullable: true),
+                    Sexual = table.Column<int>(type: "integer", nullable: true),
+                    Violence = table.Column<int>(type: "integer", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ModerationDecisions", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ModerationDecisions_CorrelationId",
+                table: "ModerationDecisions",
+                column: "CorrelationId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ModerationDecisions");
+        }
+    }
+}
