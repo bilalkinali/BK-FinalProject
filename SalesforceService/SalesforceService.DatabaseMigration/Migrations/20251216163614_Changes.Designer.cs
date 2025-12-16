d9c729c46ad143389975dcccdffdb30e1d63da8c
Bilal Kinali <biki59986@edu.ucl.dk>
2025-12-16 17:36:57 +0100

    Index and Unique Salesforce fluent api additions

--- SalesforceService/SalesforceService.DatabaseMigration/Migrations/20251216163614_Changes.Designer.cs
+++ SalesforceService/SalesforceService.DatabaseMigration/Migrations/20251216163614_Changes.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SalesforceService.Infrastructure;
@@ -11,9 +12,11 @@ using SalesforceService.Infrastructure;
 namespace SalesforceService.DatabaseMigration.Migrations
 {
     [DbContext(typeof(SalesforceContext))]
-    partial class SalesforceContextModelSnapshot : ModelSnapshot
+    [Migration("20251216163614_Changes")]
+    partial class Changes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SalesforceService/SalesforceService.DatabaseMigration/Migrations/20251216163614_Changes.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesforceService.DatabaseMigration.Migrations
+{
+    /// <inheritdoc />
+    public partial class Changes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_OutboundEvents_CorrelationId",
+                table: "OutboundEvents",
+                column: "CorrelationId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InboundEvents_SalesforceTopic_ReplayId",
+                table: "InboundEvents",
+                columns: new[] { "SalesforceTopic", "ReplayId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_OutboundEvents_CorrelationId",
+                table: "OutboundEvents");
+
+            migrationBuilder.DropIndex(
+                name: "IX_InboundEvents_SalesforceTopic_ReplayId",
+                table: "InboundEvents");
+        }
+    }
+}
